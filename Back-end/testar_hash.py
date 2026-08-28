4fe6250d57180a8b94de955c073858ed80976320
Sérgio Emídio <sergioemidiog4@gmail.com>
2026-08-27 23:46:10 -0300

    correção...

---
+++ Back-end/testar_hash.py
@@ -0,0 +1,8 @@
+﻿from passlib.context import CryptContext
+
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+HASH_DO_ENV = "$2b$12$5OaDvAABdjSwID60ykopQeF/3DTObLU/UqNHjKK.9qE2BtGjp/CBW"
+SENHA_TESTADA = "OrionProject_"
+
+print("Resultado:", pwd.verify(SENHA_TESTADA, HASH_DO_ENV))
